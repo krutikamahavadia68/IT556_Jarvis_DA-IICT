837ad9a4fe64eca60e5959dbb1f1e1c4bb61a105
Akash Thakrar <akashthakrar4@gmail.com>
2018-03-06 00:02:10 +0530

    Update svd.py

--- 201711038_akash_thakrar/svd.py
+++ 201711038_akash_thakrar/svd.py
@@ -1,18 +1,13 @@
 import pandas as pd
 import numpy as np
-
 import sklearn.preprocessing as sk
 from sklearn.decomposition import TruncatedSVD
 from sklearn.random_projection import sparse_random_matrix
 from sklearn.utils.extmath import randomized_svd
 
-df = pd.read_csv('./Ratings_10.csv', dtype={'rating': float})
-
+df = pd.read_csv('./Ratings_10_1.csv', dtype={'rating': float})
 df.loc[:,'rating'] = sk.minmax_scale(df.loc[:,'rating'] )
-
-
-
-R_df = df.pivot(index = 'user_id', columns ='book_id', values = 'rating').fillna(0)
+R_df = df.pivot(index = 'user_id', columns ='song_id', values = 'rating').fillna(0)
 
 R = R_df.as_matrix()
 user_ratings_mean = np.mean(R, axis = 1)
